b8b604267f52fbfb861882f663242fbaaf932128
Dominik Rodzim <dominik.rodzim95@gmail.com>
2019-02-13 23:08:28 +0100

    Dashboard finished

--- js/script.js
+++ js/script.js
@@ -1,68 +1,64 @@
-function toggleNotificationR(visible) {
-  document.querySelector('.notification').classList.toggle('show-on', visible)
+function toggleNotification() {
+  document.querySelector('.notification').classList.toggle('show-on')
 };
 
 document.querySelector('.notification-trigger').addEventListener('click', function(e) {
   e.preventDefault();
-  toggleNotificationR()
+  toggleNotification()
 });
 
 document.querySelector('.notification-cross').addEventListener('click', function(e) {
   e.preventDefault();
-  toggleNotificationR()
+  toggleNotification()
 });
 
 
-function toggleNotificationM(visible) {
-  document.querySelector('.notification-dropdown-m').classList.toggle('height-notification', visible)
+function toggleNotificationMobile() {
+  document.querySelector('.notification-dropdown-m').classList.toggle('height-notification')
 };
 
-function toggleNotificationRotate(visible) {
-  document.querySelector('.notification-arrow-m').classList.toggle('rotate', visible)
+function toggleNotificationRotate() {
+  document.querySelector('.notification-arrow-m').classList.toggle('rotate')
 };
 
 document.querySelector('.notification-arrow-m').addEventListener('click', function(e) {
   e.preventDefault();
-  toggleNotificationM()
+  toggleNotificationMobile()
   toggleNotificationRotate()
 });
 
 
-function toggleWalletM(visible) {
-  document.querySelector('.wallet-dropdown-m').classList.toggle('height', visible)
+function toggleWalletMobile() {
+  document.querySelector('.wallet-dropdown-m').classList.toggle('height')
 };
 
-function toggleWalletRotate(visible) {
-  document.querySelector('.wallet-mobile-arrow').classList.toggle('rotate', visible)
+function toggleWalletRotate() {
+  document.querySelector('.wallet-mobile-arrow').classList.toggle('rotate')
 };
 
 document.querySelector('.wallet-mobile-arrow').addEventListener('click', function(e) {
   e.preventDefault();
-  toggleWalletM()
+  toggleWalletMobile()
   toggleWalletRotate()
 });
 
-function toggleNotification(visible) {
-  document.querySelector('.wallet-dropdown').classList.toggle('show-on', visible)
+function toggleWallet() {
+  document.querySelector('.wallet-dropdown').classList.toggle('show-on')
 };
 
 document.querySelector('.wallet-dropdown-trigger').addEventListener('click', function(e) {
   e.preventDefault();
-  toggleNotification()
+  toggleWallet()
 });
-
-function toggleNotificationN(visible) {
-  document.querySelector('.wallet-dropdown').classList.toggle('show-on', visible)
-};
 
 document.querySelector('.wallet-arrow').addEventListener('click', function(e) {
   e.preventDefault();
-  toggleNotificationN()
+  toggleWallet()
 });
 
 
-function toggleMenuMobile(visible) {
-  document.querySelector('.sidebar').classList.toggle('menu-m-active', visible)
+function toggleMenuMobile() {
+  document.querySelector('.sidebar').classList.toggle('menu-m-active')
 }
 
 document.querySelector('#hamburger-mobile').addEventListener('click', function(e) {
@@ -70,11 +66,11 @@ document.querySelector('#hamburger-mobile').addEventListener('click', function(e
   toggleMenuMobile()
 });
 
-function toggleMenu(visible) {
-  document.querySelector('.sidebar').classList.toggle('sidebar-short', visible);
-  document.querySelector('.message').classList.toggle('show-off', visible);
-  document.querySelector('.message-short').classList.toggle('show-on', visible);
-  document.querySelector('.main-content').classList.toggle('main-content-short', visible);
+function toggleMenu() {
+  document.querySelector('.sidebar').classList.toggle('sidebar-short');
+  document.querySelector('.message').classList.toggle('show-off');
+  document.querySelector('.message-short').classList.toggle('show-on');
+  document.querySelector('.main-content').classList.toggle('main-content-short');
 }
 
 document.querySelector('#hamburger').addEventListener('click', function(e) {
@@ -122,4 +118,55 @@ function openModal(modal) {
 
 document.querySelector('.user').addEventListener('click', function(e) {
   openModal('#modalLogin')
+});
+
+document.querySelector('.user-mobile-container').addEventListener('click', function(e) {
+  openModal('#modalLogin')
+});
+
+document.querySelector('.message').addEventListener('click', function(e) {
+  openModal('#modalChat')
+});
+
+document.querySelector('.quit').addEventListener('click', function(e) {
+  openModal('#modalQuit')
+});
+
+document.querySelector('.quit-mobile').addEventListener('click', function(e) {
+  openModal('#modalQuit')
+});
+
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // 1
+    type: 'bar',
+    data: {
+        // 2
+        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+        // 3
+        datasets: [{
+            // 4
+            label: "Signups",
+            // 5
+            backgroundColor: '#8DBEC8',
+            borderColor: '#8DBEC8',
+            // 6
+            data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+        },
+        {
+            label: "FTD",
+            backgroundColor: '#F29E4E',
+            borderColor: '#F29E4E',
+            data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+        },
+        {
+            label: "Earned",
+            backgroundColor: '#71B374',
+            borderColor: '#71B374',
+            data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+            // 7
+            hidden: true,
+        }]
+    },
 });
